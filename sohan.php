85719fde9c66f71f289027cd777bb2ff2682dd38
Azharul Islam <41811919+iazharul@users.noreply.github.com>
2018-12-06 01:23:21 +0600

    Update sohan.php

--- sohan.php
+++ sohan.php
@@ -1,3 +1,6 @@
 //payment systme get url
 ini_set("allow_url_fopen", 1);
 $fb = file_get_contents($url, false);
+
+//laravel date and time formate
+ {{ date("d-m-Y || H:i A", strtotime($blog->created_at)) }}
